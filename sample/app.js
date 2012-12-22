4eeb994eb34e722c9d20e206c23e5a756cea5f73
Golo Roden <webmaster@goloroden.de>
2012-12-22 18:14:12 +0100

    Added keys to sample application.

--- sample/app.js
+++ sample/app.js
@@ -9,14 +9,14 @@ var encryptedText,
 
 console.log('original : ' + text);
 
-crypto2.readPublicKey(__dirname + '/../test/key.pub', function (publicKey) {
+crypto2.readPublicKey(__dirname + '/key.pub', function (publicKey) {
   crypto2.createPassword(function (password) {
     encryptedText = crypto2.encrypt(text, password);
     encryptedKey = crypto2.encrypt.rsa(password, publicKey);
 
     console.log('encrypted: ' + encryptedText);
 
-    crypto2.readPrivateKey(__dirname + '/../test/key.pem', function (privateKey) {
+    crypto2.readPrivateKey(__dirname + '/key.pem', function (privateKey) {
       decryptedKey = crypto2.decrypt.rsa(encryptedKey, privateKey);
       decryptedText = crypto2.decrypt(encryptedText, decryptedKey);
 
