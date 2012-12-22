7ea4639fac6295fb07b04623d1a45f244e8a8696
Golo Roden <webmaster@goloroden.de>
2012-12-22 18:13:15 +0100

    Updated the sample application.

--- sample/app.js
+++ sample/app.js
@@ -7,20 +7,20 @@ var encryptedText,
     decryptedKey,
     decryptedText;
 
-console.log('original text: ' + text);
+console.log('original : ' + text);
 
 crypto2.readPublicKey(__dirname + '/../test/key.pub', function (publicKey) {
   crypto2.createPassword(function (password) {
     encryptedText = crypto2.encrypt(text, password);
     encryptedKey = crypto2.encrypt.rsa(password, publicKey);
 
-    console.log('encryptedText: ' + encryptedText);
+    console.log('encrypted: ' + encryptedText);
 
     crypto2.readPrivateKey(__dirname + '/../test/key.pem', function (privateKey) {
       decryptedKey = crypto2.decrypt.rsa(encryptedKey, privateKey);
       decryptedText = crypto2.decrypt(encryptedText, decryptedKey);
 
-      console.log('decryptedText: ' + decryptedText);
+      console.log('decrypted: ' + decryptedText);
     });
   });
 });
