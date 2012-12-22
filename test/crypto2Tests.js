9afc7537d31c42ce2f2a3a4c642526697ae1977d
Golo Roden <webmaster@goloroden.de>
2012-12-22 01:08:25 +0100

    Added support for the AES 256 CBC algorithm.

--- test/crypto2Tests.js
+++ test/crypto2Tests.js
@@ -3,6 +3,30 @@ var assert = require('node-assertthat');
 var crypto2 = require('../lib/crypto2');
 
 suite('crypto2', function () {
+  suite('encrypt', function () {
+    suite('aes256cbc', function () {
+      test('encrypts using the AES 256 CBC encryption standard.', function () {
+        assert.that(crypto2.encrypt.aes256cbc('the native web', 'secret'), is.equalTo('6c9ae06e9cd536bf38d0f551f8150065'));
+      });
+    });
+
+    test('defaults to AES 256 CBC.', function () {
+      assert.that(crypto2.encrypt('the native web', 'secret'), is.equalTo(crypto2.encrypt.aes256cbc('the native web', 'secret')));
+    });
+  });
+
+  suite('decrypt', function () {
+    suite('aes256cbc', function () {
+      test('decrypts using the AES 256 CBC encryption standard.', function () {
+        assert.that(crypto2.decrypt.aes256cbc('6c9ae06e9cd536bf38d0f551f8150065', 'secret'), is.equalTo('the native web'));
+      });
+    });
+
+    test('defaults to AES 256 CBC.', function () {
+      assert.that(crypto2.decrypt('6c9ae06e9cd536bf38d0f551f8150065', 'secret'), is.equalTo(crypto2.decrypt.aes256cbc('6c9ae06e9cd536bf38d0f551f8150065', 'secret')));
+    });
+  });
+
   suite('hash', function () {
     suite('md5', function () {
       test('calculates the MD5 hash value.', function () {
