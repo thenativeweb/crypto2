b9c81b1a295a016760d701fc52cd1e3ae8c8354a
Golo Roden <golo.roden@thenativeweb.io>
2018-05-01 18:12:02 +0200

    Release 2.0.0.

--- dist/crypto2.js
+++ dist/crypto2.js
@@ -23,38 +23,59 @@ var crypto = require('crypto'),
 var NodeRSA = require('node-rsa'),
     promisify = require('util.promisify');
 
-var randomBytes = promisify(crypto.randomBytes),
-    readFile = typeof fs.readFile === 'function' ? promisify(fs.readFile) : (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
-  return _regenerator2.default.wrap(function _callee$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          throw new Error('Invalid operation.');
+var pbkdf2 = promisify(crypto.pbkdf2),
+    randomBytes = promisify(crypto.randomBytes);
 
-        case 1:
-        case 'end':
-          return _context.stop();
-      }
-    }
-  }, _callee, this);
-}));
+var readFile = void 0;
+
+if (typeof fs.readFile === 'function') {
+  readFile = promisify(fs.readFile);
+} else {
+  readFile = function () {
+    var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee() {
+      return _regenerator2.default.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              throw new Error('Invalid operation.');
+
+            case 1:
+            case 'end':
+              return _context.stop();
+          }
+        }
+      }, _callee, this);
+    }));
+
+    return function readFile() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+}
 
 var createPassword = function () {
-  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2() {
-    var buffer, password;
+  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(password) {
+    var length = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 32;
+    var salt, iterations, key;
     return _regenerator2.default.wrap(function _callee2$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
             _context2.next = 2;
-            return randomBytes(24);
+            return randomBytes(32);
 
           case 2:
-            buffer = _context2.sent;
-            password = buffer.toString('base64');
-            return _context2.abrupt('return', password);
+            salt = _context2.sent;
+            iterations = 10000;
+            _context2.next = 6;
+            return pbkdf2(password, salt, iterations, length, 'sha512');
 
-          case 5:
+          case 6:
+            _context2.t0 = length;
+            key = _context2.sent.toString('hex').substring(0, _context2.t0);
+            return _context2.abrupt('return', key);
+
+          case 9:
           case 'end':
             return _context2.stop();
         }
@@ -62,7 +83,7 @@ var createPassword = function () {
     }, _callee2, this);
   }));
 
-  return function createPassword() {
+  return function createPassword(_x2) {
     return _ref2.apply(this, arguments);
   };
 }();
@@ -75,7 +96,10 @@ var createKeyPair = function () {
         switch (_context3.prev = _context3.next) {
           case 0:
             /* eslint-disable id-length */
-            key = new NodeRSA({ b: 2048, e: 65537 }, { environment: 'node', signingAlgorithm: 'sha256' });
+            key = new NodeRSA({ b: 2048, e: 65537 }, {
+              environment: 'node',
+              signingAlgorithm: 'sha256'
+            });
             /* eslint-enable id-length */
 
             privateKey = key.exportKey(), publicKey = key.exportKey('public');
@@ -118,7 +142,7 @@ var readKey = function () {
     }, _callee4, this);
   }));
 
-  return function readKey(_x, _x2) {
+  return function readKey(_x3, _x4) {
     return _ref4.apply(this, arguments);
   };
 }();
@@ -145,7 +169,7 @@ var readPrivateKey = function () {
     }, _callee5, this);
   }));
 
-  return function readPrivateKey(_x3) {
+  return function readPrivateKey(_x5) {
     return _ref5.apply(this, arguments);
   };
 }();
@@ -172,7 +196,7 @@ var readPublicKey = function () {
     }, _callee6, this);
   }));
 
-  return function readPublicKey(_x4) {
+  return function readPublicKey(_x6) {
     return _ref6.apply(this, arguments);
   };
 }();
@@ -213,22 +237,26 @@ var processStream = function processStream(cipher, text, options) {
   });
 };
 
-var aes256cbcEncrypt = function () {
-  var _ref7 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee7(text, password) {
-    var cipher, encrypted;
+var createIv = function () {
+  var _ref7 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee7() {
+    var password, iv;
     return _regenerator2.default.wrap(function _callee7$(_context7) {
       while (1) {
         switch (_context7.prev = _context7.next) {
           case 0:
-            cipher = crypto.createCipher('aes-256-cbc', password);
-            _context7.next = 3;
-            return processStream(cipher, text, { from: 'utf8', to: 'hex' });
+            _context7.next = 2;
+            return randomBytes(32);
 
-          case 3:
-            encrypted = _context7.sent;
-            return _context7.abrupt('return', encrypted);
+          case 2:
+            password = _context7.sent;
+            _context7.next = 5;
+            return createPassword(password, 16);
 
           case 5:
+            iv = _context7.sent;
+            return _context7.abrupt('return', iv);
+
+          case 7:
           case 'end':
             return _context7.stop();
         }
@@ -236,25 +264,25 @@ var aes256cbcEncrypt = function () {
     }, _callee7, this);
   }));
 
-  return function aes256cbcEncrypt(_x5, _x6) {
+  return function createIv() {
     return _ref7.apply(this, arguments);
   };
 }();
 
-var aes256cbcDecrypt = function () {
-  var _ref8 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee8(text, password) {
-    var decipher, decrypted;
+var aes256cbcEncrypt = function () {
+  var _ref8 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee8(text, password, iv) {
+    var cipher, encrypted;
     return _regenerator2.default.wrap(function _callee8$(_context8) {
       while (1) {
         switch (_context8.prev = _context8.next) {
           case 0:
-            decipher = crypto.createDecipher('aes-256-cbc', password);
+            cipher = crypto.createCipheriv('aes-256-cbc', password, iv);
             _context8.next = 3;
-            return processStream(decipher, text, { from: 'hex', to: 'utf8' });
+            return processStream(cipher, text, { from: 'utf8', to: 'hex' });
 
           case 3:
-            decrypted = _context8.sent;
-            return _context8.abrupt('return', decrypted);
+            encrypted = _context8.sent;
+            return _context8.abrupt('return', encrypted);
 
           case 5:
           case 'end':
@@ -264,23 +292,27 @@ var aes256cbcDecrypt = function () {
     }, _callee8, this);
   }));
 
-  return function aes256cbcDecrypt(_x7, _x8) {
+  return function aes256cbcEncrypt(_x7, _x8, _x9) {
     return _ref8.apply(this, arguments);
   };
 }();
 
-var rsaEncrypt = function () {
-  var _ref9 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee9(text, publicKey) {
-    var key, encrypted;
+var aes256cbcDecrypt = function () {
+  var _ref9 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee9(text, password, iv) {
+    var decipher, decrypted;
     return _regenerator2.default.wrap(function _callee9$(_context9) {
       while (1) {
         switch (_context9.prev = _context9.next) {
           case 0:
-            key = new NodeRSA(publicKey);
-            encrypted = key.encrypt(text, 'base64', 'utf8');
-            return _context9.abrupt('return', encrypted);
+            decipher = crypto.createDecipheriv('aes-256-cbc', password, iv);
+            _context9.next = 3;
+            return processStream(decipher, text, { from: 'hex', to: 'utf8' });
 
           case 3:
+            decrypted = _context9.sent;
+            return _context9.abrupt('return', decrypted);
+
+          case 5:
           case 'end':
             return _context9.stop();
         }
@@ -288,21 +320,21 @@ var rsaEncrypt = function () {
     }, _callee9, this);
   }));
 
-  return function rsaEncrypt(_x9, _x10) {
+  return function aes256cbcDecrypt(_x10, _x11, _x12) {
     return _ref9.apply(this, arguments);
   };
 }();
 
-var rsaDecrypt = function () {
-  var _ref10 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee10(text, privateKey) {
-    var key, decrypted;
+var rsaEncrypt = function () {
+  var _ref10 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee10(text, publicKey) {
+    var key, encrypted;
     return _regenerator2.default.wrap(function _callee10$(_context10) {
       while (1) {
         switch (_context10.prev = _context10.next) {
           case 0:
-            key = new NodeRSA(privateKey);
-            decrypted = key.decrypt(text, 'utf8');
-            return _context10.abrupt('return', decrypted);
+            key = new NodeRSA(publicKey);
+            encrypted = key.encrypt(text, 'base64', 'utf8');
+            return _context10.abrupt('return', encrypted);
 
           case 3:
           case 'end':
@@ -312,27 +344,23 @@ var rsaDecrypt = function () {
     }, _callee10, this);
   }));
 
-  return function rsaDecrypt(_x11, _x12) {
+  return function rsaEncrypt(_x13, _x14) {
     return _ref10.apply(this, arguments);
   };
 }();
 
-var sha256Sign = function () {
+var rsaDecrypt = function () {
   var _ref11 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee11(text, privateKey) {
-    var sign, signature;
+    var key, decrypted;
     return _regenerator2.default.wrap(function _callee11$(_context11) {
       while (1) {
         switch (_context11.prev = _context11.next) {
           case 0:
-            sign = crypto.createSign('RSA-SHA256');
-            _context11.next = 3;
-            return processStream(sign, text, { from: 'utf8', to: 'utf8' });
+            key = new NodeRSA(privateKey);
+            decrypted = key.decrypt(text, 'utf8');
+            return _context11.abrupt('return', decrypted);
 
           case 3:
-            signature = sign.sign(privateKey, 'hex');
-            return _context11.abrupt('return', signature);
-
-          case 5:
           case 'end':
             return _context11.stop();
         }
@@ -340,25 +368,25 @@ var sha256Sign = function () {
     }, _callee11, this);
   }));
 
-  return function sha256Sign(_x13, _x14) {
+  return function rsaDecrypt(_x15, _x16) {
     return _ref11.apply(this, arguments);
   };
 }();
 
-var sha256Verify = function () {
-  var _ref12 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee12(text, publicKey, signature) {
-    var verify, isSignatureValid;
+var sha256Sign = function () {
+  var _ref12 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee12(text, privateKey) {
+    var sign, signature;
     return _regenerator2.default.wrap(function _callee12$(_context12) {
       while (1) {
         switch (_context12.prev = _context12.next) {
           case 0:
-            verify = crypto.createVerify('RSA-SHA256');
+            sign = crypto.createSign('RSA-SHA256');
             _context12.next = 3;
-            return processStream(verify, text, { from: 'utf8', to: 'utf8' });
+            return processStream(sign, text, { from: 'utf8', to: 'utf8' });
 
           case 3:
-            isSignatureValid = verify.verify(publicKey, signature, 'hex');
-            return _context12.abrupt('return', isSignatureValid);
+            signature = sign.sign(privateKey, 'hex');
+            return _context12.abrupt('return', signature);
 
           case 5:
           case 'end':
@@ -368,25 +396,25 @@ var sha256Verify = function () {
     }, _callee12, this);
   }));
 
-  return function sha256Verify(_x15, _x16, _x17) {
+  return function sha256Sign(_x17, _x18) {
     return _ref12.apply(this, arguments);
   };
 }();
 
-var md5 = function () {
-  var _ref13 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee13(text) {
-    var hash, hashValue;
+var sha256Verify = function () {
+  var _ref13 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee13(text, publicKey, signature) {
+    var verify, isSignatureValid;
     return _regenerator2.default.wrap(function _callee13$(_context13) {
       while (1) {
         switch (_context13.prev = _context13.next) {
           case 0:
-            hash = crypto.createHash('md5');
+            verify = crypto.createVerify('RSA-SHA256');
             _context13.next = 3;
-            return processStream(hash, text, { from: 'utf8', to: 'hex' });
+            return processStream(verify, text, { from: 'utf8', to: 'utf8' });
 
           case 3:
-            hashValue = _context13.sent;
-            return _context13.abrupt('return', hashValue);
+            isSignatureValid = verify.verify(publicKey, signature, 'hex');
+            return _context13.abrupt('return', isSignatureValid);
 
           case 5:
           case 'end':
@@ -396,19 +424,19 @@ var md5 = function () {
     }, _callee13, this);
   }));
 
-  return function md5(_x18) {
+  return function sha256Verify(_x19, _x20, _x21) {
     return _ref13.apply(this, arguments);
   };
 }();
 
-var sha1 = function () {
+var md5 = function () {
   var _ref14 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee14(text) {
     var hash, hashValue;
     return _regenerator2.default.wrap(function _callee14$(_context14) {
       while (1) {
         switch (_context14.prev = _context14.next) {
           case 0:
-            hash = crypto.createHash('sha1');
+            hash = crypto.createHash('md5');
             _context14.next = 3;
             return processStream(hash, text, { from: 'utf8', to: 'hex' });
 
@@ -424,19 +452,19 @@ var sha1 = function () {
     }, _callee14, this);
   }));
 
-  return function sha1(_x19) {
+  return function md5(_x22) {
     return _ref14.apply(this, arguments);
   };
 }();
 
-var sha256 = function () {
+var sha1 = function () {
   var _ref15 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee15(text) {
     var hash, hashValue;
     return _regenerator2.default.wrap(function _callee15$(_context15) {
       while (1) {
         switch (_context15.prev = _context15.next) {
           case 0:
-            hash = crypto.createHash('sha256');
+            hash = crypto.createHash('sha1');
             _context15.next = 3;
             return processStream(hash, text, { from: 'utf8', to: 'hex' });
 
@@ -452,21 +480,21 @@ var sha256 = function () {
     }, _callee15, this);
   }));
 
-  return function sha256(_x20) {
+  return function sha1(_x23) {
     return _ref15.apply(this, arguments);
   };
 }();
 
-var sha1hmac = function () {
-  var _ref16 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee16(text, password) {
-    var hmac, hashValue;
+var sha256 = function () {
+  var _ref16 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee16(text) {
+    var hash, hashValue;
     return _regenerator2.default.wrap(function _callee16$(_context16) {
       while (1) {
         switch (_context16.prev = _context16.next) {
           case 0:
-            hmac = crypto.createHmac('sha1', password);
+            hash = crypto.createHash('sha256');
             _context16.next = 3;
-            return processStream(hmac, text, { from: 'utf8', to: 'hex' });
+            return processStream(hash, text, { from: 'utf8', to: 'hex' });
 
           case 3:
             hashValue = _context16.sent;
@@ -480,19 +508,19 @@ var sha1hmac = function () {
     }, _callee16, this);
   }));
 
-  return function sha1hmac(_x21, _x22) {
+  return function sha256(_x24) {
     return _ref16.apply(this, arguments);
   };
 }();
 
-var sha256hmac = function () {
+var sha1hmac = function () {
   var _ref17 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee17(text, password) {
     var hmac, hashValue;
     return _regenerator2.default.wrap(function _callee17$(_context17) {
       while (1) {
         switch (_context17.prev = _context17.next) {
           case 0:
-            hmac = crypto.createHmac('sha256', password);
+            hmac = crypto.createHmac('sha1', password);
             _context17.next = 3;
             return processStream(hmac, text, { from: 'utf8', to: 'hex' });
 
@@ -508,8 +536,36 @@ var sha256hmac = function () {
     }, _callee17, this);
   }));
 
-  return function sha256hmac(_x23, _x24) {
+  return function sha1hmac(_x25, _x26) {
     return _ref17.apply(this, arguments);
+  };
+}();
+
+var sha256hmac = function () {
+  var _ref18 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee18(text, password) {
+    var hmac, hashValue;
+    return _regenerator2.default.wrap(function _callee18$(_context18) {
+      while (1) {
+        switch (_context18.prev = _context18.next) {
+          case 0:
+            hmac = crypto.createHmac('sha256', password);
+            _context18.next = 3;
+            return processStream(hmac, text, { from: 'utf8', to: 'hex' });
+
+          case 3:
+            hashValue = _context18.sent;
+            return _context18.abrupt('return', hashValue);
+
+          case 5:
+          case 'end':
+            return _context18.stop();
+        }
+      }
+    }, _callee18, this);
+  }));
+
+  return function sha256hmac(_x27, _x28) {
+    return _ref18.apply(this, arguments);
   };
 }();
 
@@ -519,6 +575,7 @@ var crypto2 = {
   readPrivateKey: readPrivateKey,
   readPublicKey: readPublicKey,
 
+  createIv: createIv,
   encrypt: aes256cbcEncrypt,
   decrypt: aes256cbcDecrypt,
 
